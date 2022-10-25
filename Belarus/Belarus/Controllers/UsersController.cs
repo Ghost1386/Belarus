534c4f52dcf1b5041641e499d359a676d9a92afc
Ghost1386 <71891421+Ghost1386@users.noreply.github.com>
2022-10-26 01:00:48 +0300

    add project files

---
+++ Belarus/Belarus/Controllers/UsersController.cs
@@ -0,0 +1,29 @@
+﻿using Belarus.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Belarus.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UsersController : ControllerBase
+{
+    [Route("post")]
+    [HttpPost]
+    public void Post([FromBody] User user)
+    {
+        if (user is not null)
+        {
+            
+        }
+    }
+    
+    [Route("post2")]
+    [HttpPost]
+    public void Post2([FromBody] User user)
+    {
+        if (user is not null)
+        {
+            
+        }
+    }
+}
---
+++ Belarus/Belarus/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace Belarus.Models;
+
+public class User
+{
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Document { get; set; }
+    public string Phone { get; set; }
+}
