8d2205855f0c4cf24b83e1c9bc02a1bf64ed011f
Ghost1386 <71891421+Ghost1386@users.noreply.github.com>
2022-10-26 20:19:53 +0300

    add appeal

--- Belarus/Belarus.BusinessLogic/Services/AppealService.cs
+++ Belarus/Belarus.BusinessLogic/Services/AppealService.cs
@@ -9,21 +9,30 @@ public class AppealService : IAppealService
 {
     public async void AppealSend(AppealDto model)
     {
-        var emailMessage = new MimeMessage();
- 
-        emailMessage.From.Add(new MailboxAddress("Электронное обращение", "lemif1008@yandex.by"));
-        emailMessage.To.Add(new MailboxAddress("", "lemif1008@yandex.by"));
-        emailMessage.Subject = model.Theme;
-        emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html)
+        try
         {
-            Text = $"{model.Text}/n {model.Mail}"
-        };
-
-        using var client = new SmtpClient();
-        await client.ConnectAsync("smtp.yandex.ru", 587, false);
-        await client.AuthenticateAsync("lemif1008@yandex.by", "gppuvgccemqdjtfd");
-        await client.SendAsync(emailMessage);
+            var emailMessage = new MimeMessage();
  
-        await client.DisconnectAsync(true);
+            emailMessage.From.Add(new MailboxAddress("Электронное обращение", "lemif1008@yandex.by"));
+            emailMessage.To.Add(new MailboxAddress("", "lemif1008@yandex.by"));
+            emailMessage.Subject = model.Theme;
+            emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html)
+            {
+                Text = $"Почта для ответа:{model.Mail}\n" +
+                       $"Текст обращения:{model.Text}"
+            };
+
+            using var client = new SmtpClient();
+            await client.ConnectAsync("smtp.yandex.ru", 587, false);
+            await client.AuthenticateAsync("lemif1008@yandex.by", "gppuvgccemqdjtfd");
+            await client.SendAsync(emailMessage);
+ 
+            await client.DisconnectAsync(true);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
     }
 }
