05d330f66fe2aa7c2e612661803c4f4bb48f793f
Dzmitry Burak <lemif1008@yandex.ru>
2022-11-13 00:48:35 +0300

    fix (Belarus): [BE,FE] fixed incorrect naming

--- Belarus/Belarus.Common/DTOs/NewsDto.cs
+++ Belarus/Belarus.Common/DTOs/NewsDto.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
 
 namespace Belarus.Common.DTOs;
 
@@ -7,10 +8,10 @@ public class NewsDto
     public string Title { get; set; }
     
     public string Text { get; set; }
-    
+
     public DateTime Date { get; set; }
     
-    public List<IFormFile> Images { get; set; }
+    public List<IFormFile> Photos { get; set; }
     
     public string VideoUrl { get; set; }
 }
--- Belarus/Belarus/ClientApp/src/pages/Admin.jsx
+++ Belarus/Belarus/ClientApp/src/pages/Admin.jsx
@@ -2,19 +2,18 @@ import React from 'react';
 import styles from './Appeal.module.scss';
 import axios from 'axios';
 
-const ADMIN_API_URL = 'http://localhost:7001/api/admin';
+const ADMIN_API_URL = 'http://localhost:7001/api/admin/';
 
 
-class Appeal extends React.Component {
+class Admin extends React.Component {
     state = {
-        date: '',
+        date: new Date(),
         title: '',
         text: '',
-        photos: '',
+        photos: [],
         videoUrl: '' 
         }
     
-        
         componentDidMount() {
             if (this.props.user) {
             const { date, title, text, photos, videoUrl } = this.props.user
@@ -29,20 +28,21 @@ class Appeal extends React.Component {
     onChange = e => {
     this.setState({ [e.target.name]: e.target.value })
     }
-    submitNew = async (e) => {
+
+    submitNew = async () => {
         const formData = new FormData();
-        
-        formData.append('date', this.state.data);
+
+        formData.append('date', this.state.date);
         formData.append('title', this.state.title);
         formData.append('text', this.state.text);
-        formData.append('photos', this.state.photos);
+        formData.append('videoUrl', this.state.videoUrl);
         
         for (let i = 0; i < this.state.photos.length; i++) {
-            formData.append('formFile', this.state.photos[i]);
+            formData.append('photos', this.state.photos[i]);
         }
 
         try {
-            const res = await axios.post(ADMIN_API_URL + '/newsCreate', formData);
+            const res = await axios.post(ADMIN_API_URL + 'newsCreate', formData);
             console.log(res);
         } catch (ex) {
             console.log(ex);
@@ -53,26 +53,26 @@ class Appeal extends React.Component {
     return  <div className={styles.container}>
     <form onSubmit={this.submitNew} className={styles.form}>
     
-    <label htmlFor="date" className={styles.form__label}>Дата</label>
-    <input type="date" name="date" className={styles.form__input} onChange={this.onChange} value={this.state.name === '' ? '' : this.state.name} />
+            <label htmlFor="date" className={styles.form__label}>Дата</label>
+            <input type="date" name="date" className={styles.form__input} onChange={this.onChange} value={this.state.date === null ? '' : this.state.date} />
     
     
-    <label htmlFor="title" className={styles.form__label}>Заголовок</label>
-    <input type="text" name="title" className={styles.form__input} onChange={this.onChange} value={this.state.mail === null ? '' : this.state.mail} />
+            <label htmlFor="title" className={styles.form__label}>Заголовок</label>
+            <input type="text" name="title" className={styles.form__input} onChange={this.onChange} value={this.state.title === null ? '' : this.state.title} />
     
     
-    <label htmlFor="text" className={styles.form__label}>Текст</label>
-    <input type="text" name="text" className={styles.form__input} onChange={this.onChange} value={this.state.theme === null ? '' : this.state.theme} />
+            <label htmlFor="text" className={styles.form__label}>Текст</label>
+            <input type="text" name="text" className={styles.form__input} onChange={this.onChange} value={this.state.text === null ? '' : this.state.text} />
     
-    <label htmlFor="videoUrl" className={styles.form__label}>Прикрепить ссылку на видео</label>
-    <input type="url" name="videoUrl" className={styles.form__input} onChange={this.onChange} value={this.state.theme === null ? '' : this.state.theme} />
+            <label htmlFor="videoUrl" className={styles.form__label}>Прикрепить ссылку на видео</label>
+            <input type="url" name="videoUrl" className={styles.form__input} onChange={this.onChange} value={this.state.videoUrl === null ? '' : this.state.videoUrl} />
     
     <label htmlFor="file" className={styles.form__label}>Прикрепить фотографии</label>
     <input type="file" multiple name="photos" onChange={this.onFileChange} className={styles.form__inputfile}/>
     
-    <button className={styles.form__button} onClick={() => {if (this.state.name === '') return alert('введите имя')}}>Отправить</button>
+            <input type="submit" />
     </form>
     </div>;
     }
     }
-    export default Appeal;
+    export default Admin;
