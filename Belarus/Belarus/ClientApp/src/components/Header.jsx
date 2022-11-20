e954a0c16be3d652bfab06f9424096f232c78f3c
PavelFrontend <pvlbk03@gmail.com>
2022-11-20 17:06:27 +0300

    changed navbar

--- Belarus/Belarus/ClientApp/src/components/Header.jsx
+++ Belarus/Belarus/ClientApp/src/components/Header.jsx
@@ -1,6 +1,6 @@
 import styles from './Header.module.scss';
 import { Link } from 'react-router-dom';
-import {Dropdown, Button, ButtonGroup} from 'react-bootstrap';
+import {Dropdown, Button, ButtonGroup, Nav, Navbar, Container} from 'react-bootstrap';
 
 const Header = () => {
     return (
@@ -14,11 +14,40 @@ const Header = () => {
             <input className={styles.input} type="text" placeholder="Поиск"></input>
             </form>
         </div>
-        <nav className={styles.nav}>
+        {/* <nav className={styles.nav}>
             <ul className={styles.nav__list}>
                 <li className={styles.nav__item}>
                  
-                <Dropdown as={ButtonGroup}>
+                           
+                </li>
+                <li className={styles.nav__item}>
+               
+                </li>
+                <li className={styles.nav__item}>
+                
+                </li>
+                <li className={styles.nav__item}>
+                
+                </li>
+                <li className={styles.nav__item}>
+                
+                </li>
+                <li className={styles.nav__item}>
+                
+                </li>
+                <li className={styles.nav__item}>
+                
+                    
+                </li>
+            </ul>
+        </nav> */}
+        <Navbar  expand="lg">
+      <Container>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home" className={styles.nav__item}>
+            <Dropdown as={ButtonGroup}>
                     
                     <Button variant="none"><Link to="/">Главая</Link></Button>
                     <Dropdown.Toggle split variant="none"  id="dropdown-custom-2" />
@@ -27,30 +56,18 @@ const Header = () => {
                     <Dropdown.Item href="/council">Совет Пинской организации</Dropdown.Item>
                     
                  </Dropdown.Menu>
-            </Dropdown>            
-                </li>
-                <li className={styles.nav__item}>
-                <Button variant="none" className={styles.btn} ><Link to="/news">Новости</Link></Button>
-                </li>
-                <li className={styles.nav__item}>
-                <Button variant="none"><Link to="/gallery">Фотогалерея</Link></Button>
-                </li>
-                <li className={styles.nav__item}>
-                <Button variant="none"><Link to="/sign">Тест</Link></Button>
-                </li>
-                <li className={styles.nav__item}>
-                <Button variant="none"><Link to="/admin">Админ</Link></Button>
-                </li>
-                <li className={styles.nav__item}>
-                <Link to="/introduction"><span>Вступить в организацию</span></Link>
-                </li>
-                <li className={styles.nav__item}>
-                <Link to="/appeal"><span>Электронное обращение</span></Link>
-                    
-                </li>
-            </ul>
-        </nav>
-
+            </Dropdown> 
+            </Nav.Link>
+            <Nav.Link href="#link" className={styles.nav__item}> <Button variant="none" className={styles.btn} ><Link to="/news">Новости</Link></Button></Nav.Link>
+            <Nav.Link href="#link" className={styles.nav__item}> <Button variant="none"><Link to="/gallery">Фотогалерея</Link></Button></Nav.Link>
+            <Nav.Link href="#link" className={styles.nav__item}> <Button variant="none"><Link to="/sign">Тест</Link></Button></Nav.Link>
+            <Nav.Link href="#link" className={styles.nav__item}> <Button variant="none"><Link to="/admin">Админ</Link></Button></Nav.Link>
+            <Nav.Link href="#link" className={styles.nav__item}> <Button variant="none"><Link to="/introduction"><span>Вступить в организацию</span></Link></Button></Nav.Link>
+            <Nav.Link href="#link" className={styles.nav__item}> <Button variant="none"><Link to="/introduction"><Link to="/appeal"><span>Электронное обращение</span></Link></Link></Button></Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
     
         </div>
     )
